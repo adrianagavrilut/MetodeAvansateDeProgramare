99fa197f90919474a5c953785ac58f8701ca6d89
Adriana Gavrilut <adriana.gavrilut21@gmail.com>
2022-10-23 21:32:45 +0300

    add c03

---
+++ 03C_10_20/Edge.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace _03C_10_20
+{
+
+    public class Edge
+    {
+        public Vertex start;
+        public Vertex end;
+
+        public Edge(string data)
+        {
+            string[] buffer = data.Split(' ');
+            start = Engine.Search(buffer[0], Engine.demo);
+            end = Engine.Search(buffer[1], Engine.demo);
+        }
+
+        public void Draw(Graphics h)
+        {
+            h.DrawLine(Pens.Red, start.location, end.location);
+        }
+    }
+}
---
+++ 03C_10_20/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _03C_10_20
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //to do: trebuie mmodificata matricea de adiacenta ca nodurile cu mai multe legaturi sa apara primele
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Engine.InitGraph(pictureBox1);
+            Engine.demo = new Graph();
+            Engine.demo.LoadFromFile(@"../../TextFile1.txt");
+            //Engine.demo.Draw(Engine.grp);
+            Engine.demo.Color();
+
+            List<string> t = Engine.demo.View(listBox2);
+            Engine.demo.Draw(Engine.grp);
+            //foreach (string s in t)
+            //{
+            //    listBox1.Items.Add(s);
+            //}
+            //listBox1.Items.Add(Engine.demo.Color());
+
+            Engine.Refresh();
+        }
+    }
+}
